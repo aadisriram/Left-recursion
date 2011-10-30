3b06270a539901fd061163973c395c774cfb968e
unknown <aadi@.(none)>
2011-10-30 10:53:19 +0530

    Eclipse project, please import

---
+++ src/remove/left/recursion/Productions.java
@@ -0,0 +1,37 @@
+package remove.left.recursion;
+
+import java.util.ArrayList;
+
+public class Productions 
+{
+	
+	public String lhs;
+	public ArrayList<String> rhs;
+	
+	public Productions()
+	{
+		lhs=new String();
+		rhs.clear();
+	}
+	
+	public Productions(String l,String r)
+	{
+		lhs=new String(l);
+		
+		String[] s=r.split("/");
+		rhs=new ArrayList<String>();
+		
+		int k=s.length;
+		
+		for(int i=0;i<k;i++)
+		{
+			rhs.add(s[i]);
+		}
+	}
+	
+	public Productions(String l,ArrayList<String> r)
+	{
+		lhs=new String(l);
+		rhs=new ArrayList<String>(r);
+	}
+}
